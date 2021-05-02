397f2447b732e05dbd7cfcc364f49f31526973bc
Robert Strandh <robert.strandh@gmail.com>
2021-05-02 15:27:34 +0200

    Test input file.

---
+++ Code/chunks.lisp
@@ -0,0 +1,14 @@
+V1
+([doc:word-component-chunk
+ :unique-id 250933051337447
+ :characters "generic"]
+ [doc:word-component-chunk
+ :unique-id 250933051463396
+ :characters "function"]
+ [doc:word-chunk
+ :unique-id 250933055319897
+ :children
+ ([doc:inline-chunk
+  :indirect-id 250933051337447]
+  [doc:inline-chunk
+  :indirect-id 250933051463396])])
