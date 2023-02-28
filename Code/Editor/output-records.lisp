2b7da6f82ab33aad37cc895cc61c07e21d48839f
Robert Strandh <robert.strandh@gmail.com>
2023-02-28 15:01:44 +0100

    Add slots for text properties of records.

--- Code/Editor/output-records.lisp
+++ Code/Editor/output-records.lisp
@@ -2,7 +2,17 @@
 
 (defclass record (clim:output-record)
   ((%parent :initarg :parent :accessor parent)
-   (%x-position :initarg :x-position :accessor x-position)))
+   (%x-position :initarg :x-position :accessor x-position)
+   ;; This slot contains a factor to be applied to the text size of
+   ;; the parent record in order to obtain the text size to be used
+   ;; for this record and its children.
+   (%text-size-factor :initarg :text-size-factor :reader text-size-factor)
+   ;; This slot contains a CLIM text family to be used for this record
+   ;; and its children.
+   (%text-family :initarg :text-family :reader text-family)
+   ;; This slot contains a CLIM text face to be used for this record
+   ;; and its children.
+   (%text-face :initarg :text-face :reader text-face)))
 
 ;;; The CLIM specification says we must handle initargs :Y-POSITION,
 ;;; and :SIZE in addition to :X-POSITION and :PARENT, so we handle
