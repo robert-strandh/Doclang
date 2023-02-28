400566bd9d25e1367409fbb6647095f67d9d6203
Robert Strandh <robert.strandh@gmail.com>
2023-02-28 14:54:19 +0100

    Define more types of output record.

--- Code/Editor/output-records.lisp
+++ Code/Editor/output-records.lisp
@@ -9,7 +9,21 @@
 ;;; them in this method, which does not do anything with them.
 (defmethod initialize-instance :after
     ((record record) &key y-position size)
+  (declare (ignore y-position size))
   nil)
 
-(defclass word-record (record clim:displayed-output-record)
-  ((%contents :initarg :contents :reader contents)))
+(defclass word-record
+    (record clim:displayed-output-record)
+  ((%contents :initarg :contents :reader contentes)))
+
+(defclass compound-record (record)
+  ((%contents :initarg :contents :accessor contents)))
+
+(defclass line-record (compound-record)
+  ())
+
+(defclass paragraph-record (compound-record)
+  ())
+
+(defclass page-record (compound-record)
+  ())
